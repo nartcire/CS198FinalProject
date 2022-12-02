ecd13d8f1dfdb500bf7019836c18564a8847c44f
Randy Nguyen <randyn@berkeley.edu>
2022-12-01 23:40:52 -0800

    added script functionality

---
+++ backend/script.mjs
@@ -0,0 +1,40 @@
+import fetch from "node-fetch";
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+let USERNAME = prompt('Enter Username: ');
+let SCORE = prompt('Enter Score: ');
+
+// Makes a post request to be sent to the DB.
+
+fetch("http://localhost:4000/user/signup", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        username: `${USERNAME}`, 
+        score: `${SCORE}`
+    })
+}).then(res => {
+    return res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('error'))
+
+// Prints the leaderboard.
+
+const getLeaderboard = () => {
+    return fetch("http://localhost:4000/user/leaderboard")
+        .then(res => res.json())
+        .then(posts => displayLeaderboard(posts))
+}
+
+async function displayLeaderboard(posts) {
+    posts.sort((p1, p2) => (p1.score < p2.score) ? 1 : (p1.score > p2.score) ? -1 : 0);
+    posts.forEach((result, i) => {
+        console.log(result.username, result.score);
+    });
+}
+
+getLeaderboard();
